dd290a9c249856136f8682564e806d625a8d4566
freialobo <freialisa@mgail.com>
2015-07-04 00:38:44 -0700

    explaining wtf is happening

--- ShubhScript/shubhify.py
+++ ShubhScript/shubhify.py
@@ -1,3 +1,6 @@
+"""this takes a string input like ' New York ' and returns it as my friend Subhankar Ghosh would use it in an instagram caption - like New York | 2015.
+This also automatically pulls the current year"""
+
 from datetime import date
 def  shubhify (before) :
     """a function that takes in a string and returns the Shubhified version"""
