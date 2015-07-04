01087bd8a1a7f7ead66cec831d684fd6cb3f1ed3
freialobo <freialisa@mgail.com>
2015-07-03 21:16:59 -0700

    shubhified!

--- ShubhScript/shubhify.py
+++ ShubhScript/shubhify.py
@@ -1,1 +1,7 @@
-def function shubhify (before) 
+from datetime import date
+def  shubhify (before) :
+    """a function that takes in a string and returns the Shubhified version"""
+    after =  before + " | " + str(date.today().year)
+    return after
+a = input( "Enter a string to be shubhified ... ")
+print (shubhify(a))
