5fe3c1ef3da9086e37b8625d410d54f145507654
freialobo <freialisa@mgail.com>
2015-08-21 13:12:32 -0400

    .

---
+++ hackerrank/data structures/linked-lists/2-insert-at-tail.java
@@ -0,0 +1,27 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+Node Insert(Node head,int data) {
+// This is a "method-only" submission. 
+// You only need to complete this method. 
+     Node temp = new Node();
+    temp.data = data; 
+    temp.next = null;
+    if (head == null){
+        head = temp; 
+    }
+    else{
+        Node current = head; 
+        while(current.next != null){
+            current = current.next; 
+        }
+        current.next = temp; 
+      }
+        return head;
+    }
